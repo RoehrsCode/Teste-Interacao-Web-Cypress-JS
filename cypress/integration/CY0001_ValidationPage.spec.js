28cdcf51534bc9c7a86bfe466c8df74c9d1d6cc0
RoehrsCode <roehrscontato@gmail.com>
2023-09-15 15:49:47 -0300

    Atualização CY0001

--- cypress/integration/CY0001_ValidationPage.spec.js
+++ cypress/integration/CY0001_ValidationPage.spec.js
@@ -1,8 +1,27 @@
 //<reference types="Cypress"/>
 
 describe('Mercado Livre Brasil - Frete Grátis no mesmo dia', function () {
-    it('Verifica o título da aplicação', function () {
+
+    beforeEach(function () {
         cy.visit('https://www.mercadolivre.com.br/')
+    }
+    )
+
+    it('Verifica o título da aplicação', function () {
         cy.title().should('be.equal', 'Mercado Livre Brasil - Frete Grátis no mesmo dia')
     })
+
+    it('Atualiza CEP do Visitante', function () {
+        cy.get('body > header > div > div.nav-header-plus-cp-wrapper.nav-area.nav-bottom-area.nav-left-area > div > a').click()
+        cy.get('#addressesForm > div > div > div > div.andes-form-control.andes-form-control--textfield.andes-form-control--default.zip-code__textfield > label > div > input').should('be.visible').type('93510210')
+        cy.get('#addressesForm > div > div > div > div.andes-form-control.andes-form-control--textfield.andes-form-control--default.zip-code__textfield > label > div > div > button > span').click()
+    }
+    )
+
+    it.only('Pesquisa um produto', function () {
+        const text = 'Pneu de Carro Aro 35 Cor Prata Fiat 2004, 2005, 2006'
+        cy.get('#cb1-edit').click().type(text, { delay: 0}).should('have.value', text)
+        cy.get('body > header > div > div.nav-area.nav-top-area.nav-center-area > form > button > div').click()
+    }
+    )
 })
